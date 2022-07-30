682fd20053cc59d1f0eb14bd604327ce6d69c7f5
tansukureysi <tansu.kureysi@gmail.com>
2022-07-30 18:24:17 +0300

    textboxtangridviewaveriaktarımı
    ürünlerin kategori, adet ve maliyetlerine göre datagridview'a aktarılması

---
+++ textbox'tan_datagridview'a_veri_aktama/textbox'tan_datagridview'a_veri_aktama/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace textbox_tan_datagridview_a_veri_aktama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text_id = new System.Windows.Forms.TextBox();
+            this.textadet = new System.Windows.Forms.TextBox();
+            this.textkategori = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textmaliyet = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(49, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(49, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adet";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(49, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Maliyet";
+            // 
+            // text_id
+            // 
+            this.text_id.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.text_id.Location = new System.Drawing.Point(161, 69);
+            this.text_id.Name = "text_id";
+            this.text_id.Size = new System.Drawing.Size(140, 22);
+            this.text_id.TabIndex = 3;
+            // 
+            // textadet
+            // 
+            this.textadet.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textadet.Location = new System.Drawing.Point(161, 193);
+            this.textadet.Name = "textadet";
+            this.textadet.Size = new System.Drawing.Size(140, 22);
+            this.textadet.TabIndex = 4;
+            // 
+            // textkategori
+            // 
+            this.textkategori.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textkategori.Location = new System.Drawing.Point(161, 125);
+            this.textkategori.Name = "textkategori";
+            this.textkategori.Size = new System.Drawing.Size(140, 22);
+            this.textkategori.TabIndex = 5;
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.White;
+            this.button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button.Location = new System.Drawing.Point(52, 388);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(104, 33);
+            this.button.TabIndex = 6;
+            this.button.Text = "EKLE";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(346, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(559, 426);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // textmaliyet
+            // 
+            this.textmaliyet.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textmaliyet.Location = new System.Drawing.Point(161, 259);
+            this.textmaliyet.Name = "textmaliyet";
+            this.textmaliyet.Size = new System.Drawing.Size(140, 22);
+            this.textmaliyet.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(49, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 14);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kategori";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(916, 450);
+            this.Controls.Add(this.textmaliyet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textkategori);
+            this.Controls.Add(this.textadet);
+            this.Controls.Add(this.text_id);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text_id;
+        private System.Windows.Forms.TextBox textadet;
+        private System.Windows.Forms.TextBox textkategori;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textmaliyet;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ textbox'tan_datagridview'a_veri_aktama/textbox'tan_datagridview'a_veri_aktama/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textbox_tan_datagridview_a_veri_aktama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable tablo = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablo.Columns.Add("ID", typeof(int));
+            tablo.Columns.Add("Kategori", typeof(string));
+            tablo.Columns.Add("Adet", typeof(int));
+            tablo.Columns.Add("Maliyet", typeof(double));
+            dataGridView1.DataSource = tablo;
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tablo.Rows.Add(text_id.Text, textkategori.Text, textadet.Text, textmaliyet.Text);
+            dataGridView1.DataSource = tablo;
+        }
+
+       
+    }
+}
